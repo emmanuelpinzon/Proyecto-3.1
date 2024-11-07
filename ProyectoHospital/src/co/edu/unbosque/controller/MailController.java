7080e38ba2300f5a5b9ffe8c5a056dd97dfd3c16
emman <emman@Santiagopinzon1>
2024-11-07 18:28:09 -0500

    holaa

--- ProyectoHospital/src/co/edu/unbosque/controller/MailController.java
+++ ProyectoHospital/src/co/edu/unbosque/controller/MailController.java
@@ -3,93 +3,44 @@ package co.edu.unbosque.controller;
 import java.util.Properties;
 
 import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import javax.swing.JOptionPane;
 
 public class MailController {
-	static final String FROM = "emmanuelpinzon16@gmail.com";
-	static final String FROMNAME="Emmanuel";
-	static final String TO = "santipin168@gmail.com";
-	static final String SMTP_USERNAME="emmanuelpinzon16@gmail.com"; 
-	static final String SMTP_PASSWORD = "bosque2023";
-	static final String CONFIGSET = "";
-	static final String HOST = "smtp.us-ashburn-1.oraclecloud.com.";
-	static final int PORT= 587;
-	static final String SUBJECT = "Envio correo desde JAVA";
-	static final String BODY = String.join(
-			 
-	           System.getProperty("line.separator"),
-	           "<h1>OCI Email Delivery test</h1>",
-	           "<p>This email was sent with OCI Email Delivery using the ",
-	           "<a href='Javamail'>https://github.com/javaee/javamail'>Javamail Package</a>",
-	          " for <a href='Javahttps://www.java.com'>Java</a>."
-	 
-	       );
-	
-	public static void main(String[]args)throws Exception {
-		Properties props = System.getProperties();
-		props.put("mail.transport.protocol", "smtp");
-	       props.put("mail.smtp.port", PORT);
-	 
-	       //props.put("mail.smtp.ssl.enable", "true"); //the default value is false if not set
-	       props.put("mail.smtp.auth", "true");
-	       props.put("mail.smtp.auth.login.disable", "true");  //the default authorization order is "LOGIN PLAIN DIGEST-MD5 NTLM". 'LOGIN' must be disabled since Email Delivery authorizes as 'PLAIN'
-	       props.put("mail.smtp.starttls.enable", "true");   //TLSv1.2 is required
-	       props.put("mail.smtp.starttls.required", "true");  //Oracle Cloud Infrastructure required
+	public static void main(String[] args) throws AddressException, MessagingException {
+	    String correo = "emmanuelpinzon16@gmail.com";
+	    String contra = "bosque2023";
+	    String correoDestino = "santipin168@gmail.com";
 
-	 
-	        // Create a Session object to represent a mail session with the specified properties.
-	       Session session = Session.getDefaultInstance(props);
-	 
-	        // Create a message with the specified information.
-	        MimeMessage msg = new MimeMessage(session);
-	        msg.setFrom(new InternetAddress(FROM,FROMNAME));
-	        msg.setRecipient(Message.RecipientType.TO, new InternetAddress(TO));
-	        msg.setSubject(SUBJECT);
-	        msg.setContent(BODY,"text/html");
-	        msg.setHeader("X-SES-CONFIGURATION-SET", CONFIGSET);
-	        
-	 
-	        // Create a transport.
-	        Transport transport = session.getTransport();
-	           
-	 
-	        // Send the message.
-	 
-	        try
-	        {
-	 
-	            System.out.println("Sending Email now...standby...");
-	 
-	 
-	            // Connect to OCI Email Delivery using the SMTP credentials specified.
-	            transport.connect(HOST, SMTP_USERNAME, SMTP_PASSWORD);    
-	 
-	            // Send email.
-	            transport.sendMessage(msg, msg.getAllRecipients());
-	            System.out.println("Email sent!");
-	 
-	        }
-	 
-	        catch (Exception ex) {
-	 
-	            System.out.println("The email was not sent.");
-	            System.out.println("Error message: " + ex.getMessage());
-	 
-	        }
-	 
-	        finally
-	 
-	        {
-	 
-	            // Close & terminate the connection.
-	            transport.close();
-	 
-	        }
-	 
-	    }
+	    Properties p = new Properties();
+	    p.put("mail.smtp.host", "smtp.gmail.com");
+	    p.setProperty("mail.smtp.starttls.enable", "true");
+	    p.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+	    p.setProperty("mail.smtp.port", "587");
+	    p.setProperty("mail.smtp.user", correo);
+	    p.setProperty("mail.smtp.auth", "true");
+
+	    Session s = Session.getDefaultInstance(p);
+	    MimeMessage mensaje = new MimeMessage(s);
+
+	    mensaje.setFrom(new InternetAddress(correo));
+	    mensaje.addRecipient(Message.RecipientType.TO, new InternetAddress(correoDestino));
+	    mensaje.setSubject("Prueba de video dia 22");
+	    mensaje.setText("Este es un mensaje que se envia desde Java");
+	    
+	    Transport t = s.getTransport("smtp");
+	    t.connect(correo, contra);
+	    t.sendMessage(mensaje, mensaje.getAllRecipients());
+	    t.close();
+
+	    JOptionPane.showMessageDialog(null, "Mensaje Enviado");
+
+	}
 	}
 
 
--- ProyectoHospital/src/co/edu/unbosque/view/MenuPrincipal.java
+++ ProyectoHospital/src/co/edu/unbosque/view/MenuPrincipal.java
@@ -1,3 +1,4 @@
+
 package co.edu.unbosque.view;
 
 import java.awt.Image;
